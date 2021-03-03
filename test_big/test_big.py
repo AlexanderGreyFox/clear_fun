78dffbbb7274c185d15f09c6e0d3a8b75233fe8e
AlexanderGreyFox <40060157+AlexanderGreyFox@users.noreply.github.com>
2021-03-03 16:30:16 +0300

    Add files via upload

---
+++ test_big/test_big.py
@@ -0,0 +1,14 @@
+import aiohttp
+import boto
+import motor
+import loguru
+import keystoneauth1
+import pandas
+import matplotlib
+import bokeh
+import theano
+
+
+def simple_function():
+    print('1')
+    return('ok')
