5261030955375cc5e262dbc5a505ed19b349252a
AlexanderGreyFox <40060157+AlexanderGreyFox@users.noreply.github.com>
2021-03-03 16:27:56 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+import test_big
+
+
+setuptools.setup(
+    name="test_big",
+    description='',
+    url='',
+    author='Serov Aleksander',
+    author_email='serov@selectel.ru',
+    packages=['test_big']
+)
